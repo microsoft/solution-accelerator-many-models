deaee77894eddd48a335b3285662928ae4a935f9
Ashley Peterson <ashley.peterson@slalom.com>
2020-01-09 10:12:37 -0500

    removing unused packages, code clean up on score.py

--- 03_Scoring/scripts/score.py
+++ 03_Scoring/scripts/score.py
@@ -1,9 +1,7 @@
 
 import pandas as pd
 import os
-import uuid
 import argparse
-import datetime
 import numpy as np
 from sklearn.externals import joblib
 from joblib import dump, load
@@ -23,17 +21,13 @@ from azureml.core.run import Run
 # Import the helper script
 from entry_script_helper import EntryScriptHelper
 
-
-# Get the information for the current Run
-thisrun = Run.get_context()
-
-# Set the log file name
+current_run = Run.get_context()
 LOG_NAME = "user_log"
 
 # Parse the arguments passed in the PipelineStep through the arguments option
 parser = argparse.ArgumentParser("split")
 parser.add_argument("--n_test_set", type=int, help="input number of predictions")
-parser.add_argument("--timestamp_column", type=str, help="model name")
+parser.add_argument("--timestamp_column", type=str, help="time column from the data")
 parser.add_argument("--output_datastore", type=str, help="datastore to upload predictions to")
 parser.add_argument("--overwrite_scoring", type=str, help="setting if the scoring files should be overwritten")
 
@@ -62,11 +56,8 @@ def run(input_data):
     predictions = pd.DataFrame()
     logger.info('making predictions...')
 
-    print('looping through data')
-    # 1. Loop through the input data
-    for idx, file in enumerate(input_data): # add the enumerate for the 12,000 files
-        u1 = uuid.uuid4()
-        mname='arima'+str(u1)[0:16]
+    for idx, file in enumerate(input_data): 
+        mname='arima'
         print(mname)
         logs = []
 
@@ -74,14 +65,14 @@ def run(input_data):
         logger.info('starting ('+file+') ' + str(date1))
         thisrun.log(mname,'starttime-'+str(date1))
 
-        # 2. Unpickle Model and Make Predictions
+        # 1. Unpickle Model and Make Predictions
         model_name = 'arima_'+str(file).split('/')[-1][:-4]
         print(model_name)
         model_path = Model.get_model_path(model_name)
         model = joblib.load(model_path)
         prediction_list, conf_int = model.predict(args.n_test_set, return_conf_int = True)
 
-        # 3. Split the data for test set
+        # 2. Split the data for test set
         data = pd.read_csv(file, header=0)
         data = data.set_index(args.timestamp_column)
         max_date = datetime.datetime.strptime(data.index.max(), '%Y-%m-%d')
@@ -92,7 +83,7 @@ def run(input_data):
         test['Predictions'] = prediction_list
         print(test.head())
 
-        # 4. Calculate Accuracy Metrics
+        # 3. Calculate Accuracy Metrics
         metrics = []
         mse = mean_squared_error(test['Quantity'], test['Predictions'])
         rmse = np.sqrt(mse)
@@ -111,7 +102,7 @@ def run(input_data):
         logger.info('accuracy metrics')
         logger.info(metrics)
 
-        # 5. Save the output back to blob storage
+        # 4. Save the output back to blob storage
         '''
         If you want to return the predictions and acutal values for each model as a seperate file, use the code below to output the results 
         of each iteration to the specified output_datastore. 
@@ -125,10 +116,10 @@ def run(input_data):
 #         scoring_dstore.upload_files([output_path +'.csv'], target_path = 'oj_scoring_' + str(run_date),
 #                                     overwrite = args.overwrite_scoring, show_progress = True)
 
-        # 6. Append the predictions to return a dataframe (optional)
+        # 5. Append the predictions to return a dataframe (optional)
         predictions = predictions.append(test)
 
-        # 7. Log Metrics
+        # 6. Log Metrics
         date2=datetime.datetime.now()
         logger.info('ending ('+str(file)+') ' + str(date2))
         
@@ -149,10 +140,7 @@ def run(input_data):
         logs.append(mape)
         logs.append(idx)
         logs.append(len(input_data))
-        logs.append(thisrun.get_status())
-
-        thisrun.log(mname,'endtime-'+str(date2))
-        thisrun.log(mname,'auc-1')
+        logs.append(current_run.get_status())
 
     resultsList.append(logs)
     return resultsList
