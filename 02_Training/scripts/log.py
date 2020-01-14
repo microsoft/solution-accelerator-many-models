05a4823f1c48cec6f2791ad5e07c516f3afe3fac
Jason Shi <xinshi93@gmail.com>
2020-01-14 11:04:01 -0800

    update based on himanshu feedback

--- 02_Training/scripts/log.py
+++ 02_Training/scripts/log.py
@@ -16,24 +16,25 @@ parser.add_argument("--experiment", type=str, help="input experiment name")
 parser.add_argument("--overwrite_logs", type=str, help="input overwrite logs True or False")
 parser.add_argument("--pipeline_output_name", type=str, help="input ParralelRunStep output name")
 
-args, unknown = parser.parse_known_args()
-print("Argument1 (ParallelRunStep_name): %s" % args.ParallelRunStep_name)
-print("Argument2 (datastore): %s" % args.datastore)
-print("Argument3 (experiment): %s" % args.experiment)
-print("Argument4 (overwrite_logs): %s" % args.overwrite_logs)
-print("Argument5 (pipeline_output_name): %s" % args.pipeline_output_name)
+args, _ = parser.parse_known_args()
+
+print("Argument1 ParallelRunStep_name: {}".format(args.ParallelRunStep_name))
+print("Argument2 datastore: {}".format(args.datastore))
+print("Argument3 experiment: {}".format(args.experiment))
+print("Argument4 overwrite_logs: {}".format(args.overwrite_logs))
+print("Argument5 pipeline_output_name: {}".format(args.pipeline_output_name))
 
 # set workspace and experiment
-thisrun = Run.get_context()
-ws = thisrun.experiment.workspace
+current_run = Run.get_context()
+ws = current_run.experiment.workspace
 experiment = Experiment(ws, args.experiment)
 
 # retrieve the log file
-pipeline_runId = thisrun.get_details()['properties']['azureml.pipelinerunid']
+pipeline_runId = current_run.get_details()['properties']['azureml.pipelinerunid']
 pipeline_run = PipelineRun(experiment, pipeline_runId)
 step_run = pipeline_run.find_step_run(args.ParallelRunStep_name)[0]
 prediction_output = step_run.get_output_data(args.pipeline_output_name)
-prediction_output.download(local_path="logs")
+prediction_output.download(local_path = "logs")
 print('Downloaded the log file of Pipeline Id: '+pipeline_runId)
 
 # check the log file path
@@ -44,8 +45,8 @@ for root, dirs, files in os.walk("logs"):
             print ('Log file path: ' + result_file)
 
 # read the file and clean up data
-df_log = pd.read_csv(result_file, converters={0: lambda x: x.strip("["),10: lambda x: x.strip("]")}, delimiter=",", header=None)
-df_log.columns=['Store','Brand','ModelType','FileName','ModelName','StartTime','EndTime','Duration','Index','BatchSize','Status']
+df_log = pd.read_csv(result_file, converters = {0: lambda x: x.strip("["),10: lambda x: x.strip("]")}, delimiter = ",", header = None)
+df_log.columns = ['Store','Brand','ModelType','FileName','ModelName','StartTime','EndTime','Duration','Index','BatchSize','Status']
 df_log['Store'] = df_log['Store'].apply(str).str.replace("'", '')
 df_log['Brand'] = df_log['Brand'].apply(str).str.replace("'", '')
 df_log['ModelType'] = df_log['ModelType'].apply(str).str.replace("'", '')
@@ -61,10 +62,10 @@ print ('Read and cleaned the log file')
 
 # save the log file
 output_path = os.path.join('./logs/', 'training_log')
-df_log.to_csv(path_or_buf=output_path + '.csv', index = False)
+df_log.to_csv(path_or_buf = output_path + '.csv', index = False)
 print('Saved the training_log.csv')
 
 # upload the log file
 log_dstore = Datastore(ws, args.datastore)
-log_dstore.upload_files(['./logs/training_log'+'.csv'], target_path='training_log_'+str(datetime.datetime.now().date()), overwrite=args.overwrite_logs, show_progress=True)
+log_dstore.upload_files(['./logs/training_log'+'.csv'], target_path = 'training_log_'+str(datetime.datetime.now().date()), overwrite = args.overwrite_logs, show_progress = True)
 print('Uploaded the training_log.csv')
