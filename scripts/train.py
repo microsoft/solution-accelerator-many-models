61a289ca7f01e9d20bcd746478f64d38239ec27a
Ashley Peterson <ashley.peterson@slalom.com>
2020-02-27 20:56:01 -0500

    added model_type arg, changed model to linear regression

--- scripts/train.py
+++ scripts/train.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 from azureml.core import Run
-import pmdarima as pm
 import pandas as pd
 import numpy as np
 import os
@@ -12,18 +11,18 @@ from datetime import timedelta
 from sklearn.externals import joblib
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from entry_script import EntryScript
+from sklearn import LinearRegression 
 
 # 0.0 Parse input arguments
 parser = argparse.ArgumentParser("split")
 parser.add_argument("--target_column", type=str, help="input target column")
 parser.add_argument("--n_test_periods", type=int, help="input number of test periods")
 parser.add_argument("--timestamp_column", type=str, help="input timestamp column")
-parser.add_argument("--stepwise_training", type=str, help="input stepwise training True or False")
+parser.add_argument("--model_type", type=str, help="input model type")
 
 args, _ = parser.parse_known_args()
 
 current_run = None
-
 
 def init():
     global current_run
@@ -35,12 +34,18 @@ def run(input_data):
     entry_script = EntryScript()
     logger = entry_script.logger
     os.makedirs('./outputs', exist_ok=True)
-    result_list = []
+    logger.info('Train and Score models')
+    resultList = []
+    log_list = []
 
     # 2.0 Read in the data file
     for idx, csv_file_path in enumerate(input_data):
+        logs = []
+        date1 = datetime.datetime.now()
+        logger.info('starting ('+csv_file_path+') '+ str(date1)
+        
         file_name = os.path.basename(csv_file_path)[:-4]
-        model_name = 'arima_' + file_name
+        model_name = args.model_type + file_name
         store_name = file_name.split('_')[0]
         brand_name = file_name.split('_')[1]
 
@@ -48,61 +53,109 @@ def run(input_data):
         logger.info(data.head())
 
         # 3.0 Split the data into train and test sets based on dates
-        data = data.set_index(args.timestamp_column)
-        max_date = datetime.datetime.strptime(data.index.max(), '%Y-%m-%d')
-        split_date = max_date - timedelta(days=7 * args.n_test_periods)
-        data.index = pd.to_datetime(data.index)
-        train = data[data.index <= split_date]
-        test = data[data.index > split_date]
+        try: 
+            data = data.set_index(args.timestamp_column)
+            max_date = datetime.datetime.strptime(data.index.max(), '%Y-%m-%d')
+            split_date = max_date - timedelta(days=7 * args.n_test_periods)
+            data.index = pd.to_datetime(data.index)
+            train = data[data.index <= split_date]
+            test = data[data.index > split_date]
+            
+            y_train = train['target']
+            y_test = test['target']
+            X_train = train.drop('target', axis = 1)
+            x_test = test.drop('target', axis = 1)
 
-        # 4.0 Train the model
-        model = pm.auto_arima(train[args.target_column],
-                start_p=0,
-                start_q=0,
-                test='adf', # default stationarity test is kpps
-                max_p=3,
-                max_d=2,
-                max_q=3,
-                m=3, # number of observations per seasonal cycle
-                seasonal=True,
-                information_criterion='aic',
-                trace=True, # prints status on the fits
-                stepwise=args.stepwise_training, # this increments instead of doing a grid search
-                suppress_warnings=True,
-                out_of_sample_size=16
-        )
-        model = model.fit(train[args.target_column])
+           # 4.0 Train the model
+            model = LinearRegression().fit(X_train, y_train)
+ 
+            logger.info('done training')
+            print('Trained ' + model_name)
 
-        # 5.0 Save the model
-        joblib.dump(model, filename=os.path.join('./outputs/', model_name))
+            # 5.0 Save the model
+            logger.info('done training')
+            joblib.dump(model, filename=os.path.join('./outputs/', model_name))
 
-        # 6.0 Register the model to the workspace
-        current_run.upload_file(model_name, os.path.join('./outputs/', model_name))
+            # 6.0 Register the model to the workspace
+            try: 
+                current_run.upload_file(model_name, os.path.join('./outputs/', model_name))
+            except:
+                logger.info('dont need to upload model')
+            logger.info('register model, skip the outputs prefix')
+                        
+            tags_dict = {'Store': store_name, 'Brand': brand_name, 'ModelType': args.model_type}
+            current_run.register_model(model_path = model_name, model_name = model_name, model_framework = args.model_type, tags = tags_dict)
 
-        tags_dict = {'Store': store_name, 'Brand': brand_name, 'ModelType': 'ARIMA'}
-        current_run.register_model(model_path = model_name, model_name = model_name, model_framework = 'pmdarima',
-                                   tags = tags_dict)
+            # 7.0 Make predictions on test set
+            prediction_list = model.predict(X_test)
+        
+            # 8.0 Insert prediciotns to test set 
+            test['Predictions'] = prediction_list
+            print(test.head())
+            print('Predictions added ' + model_name)
 
-        # 7.0 Make predictions on test set
-        prediction_list, conf_int = model.predict(args.n_test_periods, return_conf_int=True)
+            # 9.0 Calculate accuracy metrics
+            metrics = []
+            mse = mean_squared_error(test[args.target_column], test['Predictions'])
+            rmse = np.sqrt(mse)
+            mae = mean_absolute_error(test[args.target_column], test['Predictions'])
+            act, pred = np.array(test[args.target_column]), np.array(test['Predictions'])
+            mape = np.mean(np.abs((act - pred) / act) * 100)
+            metrics.append(mse)
+            metrics.append(rmse)
+            metrics.append(mae)
+            metrics.append(mape)
+            print('Calculated accuracy metrics  ' + model_name)
+            print(metrics)
+                    
+            # 9.1 Log metrics
+            logger.info('accuracy metrics')
+            logger.info(metrics) 
+                        
+            # 10.0 Log the run 
+            date2 = datetime.datetime.now()
+                logs.append(store_name)
+                logs.append(brand_name)
+                logs.append(args.model_type)
+                logs.append(file_name)
+                logs.append(model_name)
+                logs.append(str(date1))
+                logs.append(str(date2))
+                logs.append(str(date2-date1))
+                logs.append(mse)
+                logs.append(rmse)
+                logs.append(mae)
+                logs.append(mape)
+                logs.append(idx)
+                logs.append(len(input_data))
+                logs.append(current_run.get_status())
 
-        # 9.0 Calculate accuracy metrics
-        metrics = []
-        mse = mean_squared_error(test['Quantity'], prediction_list)
-        rmse = np.sqrt(mse)
-        mae = mean_absolute_error(test['Quantity'], prediction_list)
-        act, pred = np.array(test['Quantity']), np.array(prediction_list)
-        mape = np.mean(np.abs((act - pred) / act) * 100)
+                log_list.append(logs)
+            logger.info('ending (' + csv_file_path + ') ' + str(date2))
 
-        # 10.0 Log metrics
-        current_run.log(model_name + '_aic', model.aic())
-        current_run.log(model_name + '_mse', mse)
-        current_run.log(model_name + '_rmse', rmse)
-        current_run.log(model_name + '_mae', mae)
-        current_run.log(model_name + '_mape', mape)
-        date2 = datetime.datetime.now()
+        # 10.1 Log error if occurred 
+        except (ValueError, UnboundLocalError, NameError, ModuleNotFoundError, AttributeError, ImportError, FileNotFoundError, KeyError) as error:
+            date2 = datetime.datetime.now()
+            error_message = 'Failed to score the model. ' + 'Error message: ' + str(error)
 
-        logger.info('ending (' + csv_file_path + ') ' + str(date2))
+            logs.append(store_name)
+            logs.append(brand_name)
+            logs.append('ARIMA')
+            logs.append(file_name)
+            logs.append(model_name)
+            logs.append(str(date1))
+            logs.append(str(date2))
+            logs.append(str(date2-date1))
+            logs.append('Null')
+            logs.append('Null')
+            logs.append('Null')
+            logs.append('Null')
+            logs.append(idx)
+            logs.append(len(input_data))
+            logs.append(error_message)
 
-        result_list.append(True)
-    return result_list
+            logger.info('ending ('+csv_file_path+') ' + str(date2))
+            log_list.append(logs)
+
+    resultList.extend(log_list)
+    return resultList
