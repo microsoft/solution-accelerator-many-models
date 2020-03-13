495548be184a81dcdc0581fb8909e9eb2b92fcda
Aniththa <anumamah@microsoft.com>
2020-03-13 09:47:53 -0700

    added forecasting notebook

--- AutoML/02b_Forecasting_Pipeline/02_Forecasting_Pipeline/helper.py
+++ AutoML/02b_Forecasting_Pipeline/02_Forecasting_Pipeline/helper.py
@@ -1,22 +1,14 @@
-import argparse
-import json
 import sys
-
 sys.path.append("..")
 
 
-def write_automl_settings_to_file(automl_settings):
-    with open('scripts\\automlconfig.json', 'w', encoding='utf-8') as f:
-        json.dump(automl_settings, f, ensure_ascii=False, indent=4)
-
-
-def build_parallel_run_config(train_env, compute, nodecount, workercount, timeout):
+def build_parallel_run_config_for_forecasting(train_env, compute, nodecount, workercount, timeout):
     from azureml.contrib.pipeline.steps import ParallelRunConfig
     from common.scripts.helper import validate_parallel_run_config
     parallel_run_config = ParallelRunConfig(
         source_directory='./scripts',
-        entry_script='trainAutoML.py',
-        mini_batch_size="1",  # do not modify this setting
+        entry_script='forecast.py',
+        mini_batch_size="10",  # do not modify this setting
         run_invocation_timeout=timeout,
         error_threshold=10,
         output_action="append_row",
