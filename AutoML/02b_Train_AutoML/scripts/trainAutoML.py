f1288b73404e35f69938a57aaeb16de2bac6e146
Aniththa <anumamah@microsoft.com>
2020-03-17 16:11:16 -0700

    updated files

--- AutoML/02b_Train_AutoML/scripts/trainAutoML.py
+++ AutoML/02b_Train_AutoML/scripts/trainAutoML.py
@@ -5,7 +5,6 @@ import uuid
 from azureml.core.dataset import Dataset
 from azureml.core.model import Model
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 import argparse
@@ -17,7 +16,6 @@ import azureml.automl.core
 from automl.client.core.common import constants
 import datetime
 from entry_script_helper import EntryScriptHelper
-from forecasting_helper import align_outputs
 import logging
 
 from sklearn.externals import joblib
@@ -57,16 +55,6 @@ print("group_column_names: {}".format(group_column_names))
 print("grain_column_names: {}".format(grain_column_names))
 
 
-def split_last_n_by_grain(df):
-    """Group df by grain and split on last n rows for each group."""
-    df_grouped = (df.sort_values(timestamp_column)  # Sort by ascending time
-                  .groupby(grain_column_names, group_keys=False))
-    print(df_grouped)
-    df_head = df_grouped.apply(lambda dfg: dfg.iloc[:-n_test_periods])
-    df_tail = df_grouped.apply(lambda dfg: dfg.iloc[-n_test_periods:])
-    return df_head, df_tail
-
-
 def init():
     EntryScriptHelper().config(LOG_NAME)
     logger = logging.getLogger(LOG_NAME)
@@ -75,61 +63,26 @@ def init():
     logger.info("init()")
 
 
-def train_model(data, file_name):
-    print(data.head())
-    if automl_settings['task'] == 'forecasting':
-        train, test = split_last_n_by_grain(data)
-    else:
-        train, test = train_test_split(data, test_size=0.2, random_state=0)
-
-    train.to_csv("./{}_train.csv".format(file_name), index=None, header=True)
-    test.to_csv("./{}_test.csv".format(file_name), index=None, header=True)
-
+def train_model(csv_file_path):
+    file_name = csv_file_path.split('/')[-1][:-4]
+    print(file_name)
+    print (csv_file_path)
     ws = current_step_run.experiment.workspace
     datastore = ws.get_default_datastore()
-    datastore.upload_files(files=["./{}_train.csv".format(file_name), "./{}_test.csv".format(file_name)],
-                           target_path='dataset/', overwrite=True, show_progress=True)
 
-    train_dataset = Dataset.Tabular.from_delimited_files(path=datastore.path("dataset/{}_train.csv".format(file_name)))
+    datastore.upload_files(files=[csv_file_path], target_path='dataset/',
+                           overwrite=True, show_progress=True)
+    train_dataset = Dataset.Tabular.from_delimited_files(path=datastore.path("dataset/{}.csv".format(file_name)))
     automl_config = AutoMLConfig(training_data=train_dataset,
                                  **automl_settings)
 
     local_run = current_step_run.submit_child(automl_config, show_output=True)
     print(local_run)
-    best_run, fitted_model = local_run.get_output()
+    fitted_model = local_run.get_output()
 
     u1 = uuid.uuid4()
     model_name = 'automl_' + str(u1)[0:16]
     return fitted_model, model_name, local_run
-
-
-def test_model(fitted_model, file_name, model_name):
-    ws = current_step_run.experiment.workspace
-    datastore = ws.get_default_datastore()
-    test_dataset = Dataset.Tabular.from_delimited_files(path=datastore.path("dataset/{}_test.csv".format(file_name)))
-    test = test_dataset.to_pandas_dataframe()
-    X_test = test
-    y_test = X_test.pop(target_column).values
-    print("test model head")
-    print(X_test.head())
-    y_predictions, X_trans = fitted_model.forecast(X_test)
-    print('Made predictions on  ' + model_name)
-
-    # Insert predictions to test set
-    test['Predictions'] = y_predictions
-    print(test.head())
-    print('Inserted predictions ' + model_name)
-
-    df_all = align_outputs(y_predictions, X_trans, X_test, y_test, target_column)
-
-    scores = {}
-    if automl_settings['task'] == 'forecasting':
-        from azureml.automl.core._vendor.automl.client.core.common import metrics as mt
-        scores = mt.compute_metrics_regression(df_all['predicted'], df_all[target_column],
-                                               list(constants.Metric.SCALAR_REGRESSION_SET),
-                                               None, None, None)
-
-    return scores
 
 
 def run(input_data):
@@ -142,13 +95,14 @@ def run(input_data):
         logs = []
         date1 = datetime.datetime.now()
         logger.info('start (' + file + ') ' + str(datetime.datetime.now()))
-        data = pd.read_csv(file)
+        data = pd.read_csv(file).head(5)
         csv_file_path = file
 
         file_name = csv_file_path.split('/')[-1][:-4]
+
         try:
             # train model
-            fitted_model, model_name, current_run = train_model(data, file_name)
+            fitted_model, model_name, current_run = train_model(csv_file_path)
             logger.info('done training')
             print('Trained best model ' + model_name)
 
@@ -164,15 +118,6 @@ def run(input_data):
 
             current_run.register_model(model_name=model_name, description='AutoML', tags=tags_dict)
             print('Registered ' + model_name)
-
-            scores = test_model(fitted_model, file_name, model_name)
-            print("[Test data scores]\n")
-
-            for key, value in scores.items():
-                print('{}:   {:.3f}'.format(key, value))
-                logger.info('{}:   {:.3f}'.format(key, value))
-
-            logger.info('Calculated metrics')
 
             date2 = datetime.datetime.now()
 
@@ -193,7 +138,7 @@ def run(input_data):
         except (ValueError, UnboundLocalError, NameError, ModuleNotFoundError, AttributeError, ImportError,
                 FileNotFoundError, KeyError) as error:
             date2 = datetime.datetime.now()
-            error_message = 'Failed to score the model. ' + 'Error message: ' + str(error)
+            error_message = 'Failed to train the model. ' + 'Error message: ' + str(error)
 
             logs.append('AutoML')
             logs.append(file_name)
