36b9f447d88a265a8d00521123b53e7d6a18486b
María Medina <mariamedp@gmail.com>
2020-05-21 08:43:56 +0200

    Training code build pipeline

--- Custom_Script/mlops-pipelines/README.md
+++ Custom_Script/mlops-pipelines/README.md
@@ -25,4 +25,24 @@ The setup pipeline will:
     - Set up the Azure Machine Learning worskpace, creating a compute target and attaching the AKS cluster.
     - Download as many files as you specified in the DATASET_MAXFILES variable and register them as a dataset in AML.
 
-Create the Pipeline as in [here](https://github.com/microsoft/MLOpsPython/blob/master/docs/getting_started.md#create-the-iac-pipeline), selecting branch **``feature/mlops``** and setting the path to [/Custom_Script/mlops_pipelines/1-setup/setup-pipeline.yml](1-setup/setup-pipeline.yml).
+Create the pipeline as in [here](https://github.com/microsoft/MLOpsPython/blob/master/docs/getting_started.md#create-the-iac-pipeline), selecting branch **``feature/mlops``** and setting the path to [/Custom_Script/mlops-pipelines/1-setup/setup-pipeline.yml](1-setup/setup-pipeline.yml).
+
+## 2. Training Code Build Pipeline
+
+The training code build pipeline will:
+    - Create an Azure Machine Learning Pipeline that will train many models in parallel using the [train](scripts/train.py) script.
+    - Publish the AML Pipeline into the AML workspace so it's ready to use whenever we want to retrain.
+
+Before creating the Azure DevOps pipeline:
+
+- Make sure the [AML extension](https://marketplace.visualstudio.com/items?itemName=ms-air-aiagility.vss-services-azureml) is installed in the Azure DevOps organization.
+
+- Create an **Azure Resource Manager** [service connection](https://docs.microsoft.com/en-us/azure/devops/pipelines/library/service-endpoints?view=azure-devops&tabs=yaml#create-a-service-connection) to access the Machine Learning Workspace you created in the setup pipeline before. As you did before, mark the option to grant access permission to all pipelines, and copy the name as you'll need it in the next step.
+
+- Modify the **``manymodels-vg``** variable group you created before, and add this new variable:  called **``manymodels-vg``**, 
+
+| Variable Name               | Short description |
+| --------------------------- | ----------------- |
+| SERVICECONNECTION_WORKSPACE | Name of the connection to the AML Workspace you have just created |
+
+Then, create the pipeline as you did before, selecting branch **``feature/mlops``** and setting the path to [/Custom_Script/mlops-pipelines/2-training-code-build/training-code-build-pipeline.yml](2-training-code-build/training-code-build-pipeline.yml).
---
+++ Custom_Script/mlops-pipelines/scripts/create_training_pipeline.py
@@ -0,0 +1,119 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import pathlib
+import argparse
+from azureml.core import Workspace, Experiment, Dataset, Environment
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.compute import AmlCompute
+from azureml.pipeline.core import Pipeline, PipelineData, PublishedPipeline
+from azureml.contrib.pipeline.steps import ParallelRunConfig, ParallelRunStep
+
+
+def create_training_pipeline(ws, pipeline_name, pipeline_version, dataset_name, compute_name):
+
+    # Get input dataset
+    dataset = Dataset.get_by_name(ws, name=dataset_name)
+    dataset_input = dataset.as_named_input(dataset_name)
+
+    # Set output
+    datastore = ws.get_default_datastore()
+    output_dir = PipelineData(name='training_output', datastore=datastore)
+
+    # Set up ParallelRunStep
+    parallel_run_config = get_parallel_run_config(ws, dataset_name, compute_name)
+    parallel_run_step = ParallelRunStep(
+        name='many-models-parallel-training',
+        parallel_run_config=parallel_run_config,
+        inputs=[dataset_input],
+        output=output_dir,
+        allow_reuse=False,
+        arguments=[
+            '--target_column', 'Quantity', 
+            '--n_test_periods', 6, 
+            '--forecast_granularity', 7, 
+            '--timestamp_column', 'WeekStarting', 
+            '--model_type', 'lr'
+        ]
+    )
+
+    # Create the pipeline
+    train_pipeline = Pipeline(workspace=ws, steps=[parallel_run_step])
+    train_pipeline.validate()
+    
+    # Publish it and replace old pipeline
+    disable_old_pipelines(ws, pipeline_name)
+    published_pipeline = train_pipeline.publish(
+        name=pipeline_name,
+        description="Many Models training/retraining pipeline",
+        version=pipeline_version,
+        continue_on_step_failure=False
+    )
+
+    return published_pipeline.id
+
+
+def get_parallel_run_config(ws, dataset_name, compute_name, processes_per_node=8, node_count=3, timeout=300):
+    
+    # Configure environment for ParallelRunStep
+    train_env = Environment(name='many_models_environment')
+    train_conda_deps = CondaDependencies.create(pip_packages=['joblib', 'sklearn'])
+    train_env.python.conda_dependencies = train_conda_deps
+    
+    # Get the compute target
+    compute = AmlCompute(ws, compute_name)
+    
+    # Set up ParallelRunStep configuration
+    parallel_run_config = ParallelRunConfig(
+        source_directory='Custom_Script/scripts/',
+        entry_script='train.py',
+        mini_batch_size='1',
+        run_invocation_timeout=timeout,
+        error_threshold=25,
+        output_action='append_row',
+        environment=train_env,
+        process_count_per_node=processes_per_node,
+        compute_target=compute,
+        node_count=node_count
+    )
+    
+    return parallel_run_config
+
+
+def disable_old_pipelines(ws, pipeline_name):
+    for pipeline in PublishedPipeline.list(ws):
+        if pipeline.name == pipeline_name:
+            pipeline.disable()
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--subscription-id', required=True, type=str)
+    parser.add_argument('--resource-group', required=True, type=str)
+    parser.add_argument('--workspace-name', required=True, type=str)
+    parser.add_argument('--name', required=True, type=str)
+    parser.add_argument('--version', required=True, type=str)
+    parser.add_argument('--dataset', type=str, default='oj_sales_data')
+    parser.add_argument('--compute', type=str, default='cpu-compute')
+    args_parsed = parser.parse_args(args)
+    return args_parsed
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Connect to workspace
+    ws = Workspace.get(
+        name=args.workspace_name,
+        subscription_id=args.subscription_id,
+        resource_group=args.resource_group
+    )
+
+    pipeline_id = create_training_pipeline(
+        ws, 
+        pipeline_name=args.name, 
+        pipeline_version=args.version, 
+        dataset_name=args.dataset,
+        compute_name=args.compute
+    )
+    print('Training pipeline {} version {} published with ID {}'.format(args.name, args.version, pipeline_id))
