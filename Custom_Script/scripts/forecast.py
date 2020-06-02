9655d168a0e46fb9b5aeb0ff3979aff20143200a
María Medina <mariamedp@gmail.com>
2020-06-02 17:04:25 +0200

    Use utils forecasting methods in batch forecasting

--- Custom_Script/scripts/forecast.py
+++ Custom_Script/scripts/forecast.py
@@ -9,6 +9,9 @@ from azureml.core.model import Model
 from azureml.core.run import Run
 import datetime
 
+from utils.forecasting import format_prediction_data, update_prediction_data
+
+
 # 0.0 Parse input arguments
 parser = argparse.ArgumentParser("split")
 parser.add_argument("--forecast_horizon", type=int, help="input number of predictions")
@@ -28,18 +31,18 @@ def run(input_data):
     # 2.0 Iterate through input data
     for idx, csv_file_path in enumerate(input_data):
         file_name = os.path.basename(csv_file_path)[:-4]
-        model_name = args.model_type + '_' + file_name
         store_name = file_name.split('_')[0]
         brand_name = file_name.split('_')[1]
 
         # 3.0 Set up data to predict on
         data = pd.read_csv(csv_file_path, header = 0)
         data[args.timestamp_column] = data[args.timestamp_column].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-        date_list = pd.date_range(args.starting_date, periods=args.forecast_horizon, freq=args.date_freq)
 
-        prediction_df = pd.DataFrame()
-        prediction_df['Date'] = date_list 
-        prediction_df['Week_Day'] = prediction_df['Date'].apply(lambda x: x.weekday())                             
+        prediction_df = format_prediction_data(
+            data, args.forecast_horizon, args.date_freq,
+            timestamp_column=args.timestamp_column,
+            value_column=args.target_column
+        )
 
         # 4.0 Unpickle model and make predictions
         ws = Run.get_context().experiment.workspace
@@ -49,39 +52,14 @@ def run(input_data):
         model_path = models[0].download()
         model = joblib.load(model_path)
 
-        
-        prediction_list = []
-        i = 0
-        for date in prediction_df['Date']:
-       
-            x_pred = prediction_df[prediction_df['Date'] == date]
-    
-        
-            if i >= 1: 
-                x_pred['lag_1'] = prediction_list[i-1]
-            else: 
-                x_pred['lag_1'] = data[args.target_column].iloc[-1]
-            if i >= 2: 
-                x_pred['lag_2'] = prediction_list[i-2]
-            else: 
-                x_pred['lag_2'] = data[args.target_column].iloc[-2+i]
-            if i >= 3: 
-                x_pred['lag_3'] = prediction_list[i-3]
-            else: 
-                x_pred['lag_3'] = data[args.target_column].iloc[-3+i]
+        for i in range(len(prediction_df)):
+            x_pred = prediction_df.loc[prediction_df.index == i].drop(columns=['Date', 'Prediction'])
+            y_pred = model.predict(x_pred)[0]
+            prediction_df = update_prediction_data(prediction_df, i, y_pred)
 
-            y_pred = model.predict(x_pred.drop(columns=['Date']))
-
-
-            prediction_list.append(y_pred[0])
-            i += 1
-
-        prediction_df['Prediction'] = prediction_list
         prediction_df['Store'] = store_name
         prediction_df['Brand'] = brand_name
-        prediction_df.drop(columns=['Week_Day'], inplace=True)
         
-
-        results = results.append(prediction_df)
+        results = results.append(prediction_df[['Date', 'Prediction', 'Store', 'Brand']])
 
     return results
