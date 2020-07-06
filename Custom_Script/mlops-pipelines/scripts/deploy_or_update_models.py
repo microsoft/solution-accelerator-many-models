c997c5fd403fdb3a8aa3df0779584f5d9ed5de29
María Medina <mariamedp@gmail.com>
2020-07-06 12:04:49 +0200

    Delete old webservices in deployment

--- Custom_Script/mlops-pipelines/scripts/deploy_or_update_models.py
+++ Custom_Script/mlops-pipelines/scripts/deploy_or_update_models.py
@@ -22,13 +22,19 @@ def main(ws, config_file, routing_model_name,
     deployment_config = get_deployment_config(config_file, aks_target)
         
     # Get deployed models
-    models_deployed = get_deployed_models(routing_model_name)
+    models_deployed, existing_services = get_deployed_models(ws, routing_model_name)
 
-    # Get groups to deploy or update
+    # Get groups to deploy or update and old services to be deleted
     all_groups = get_models_in_groups(grouping_tags=grouping_tags, sorting_tags=sorting_tags,
                                       exclude_names=[routing_model_name], container_size=container_size)
-    groups_new, groups_updated = split_groups_new_updated(all_groups, models_deployed)
+    groups_new, groups_updated, oldgroups_delete = split_groups(all_groups, models_deployed)
 
+    # Delete old services
+    for group_info in oldgroups_delete:
+        service_name = group_info['webservice']
+        existing_services[service_name].delete()
+        for model_name in group_info['models']:
+            del models_deployed[model_name]
 
     deployments = []
 
@@ -39,10 +45,9 @@ def main(ws, config_file, routing_model_name,
     
     # Launch webservice updates
     for group_name, group_models in groups_updated.items():
-        service = deploy_model_group(ws, group_name, group_models, deployment_config, name_prefix=service_prefix, update=True)
+        service = deploy_model_group(ws, group_name, group_models, deployment_config, existing_services, name_prefix=service_prefix)
         deployments.append({ 'service': service, 'group': group_name, 'models': group_models })
     
-
     # Wait for deployments to finish
     for deployment in deployments:
         
@@ -71,7 +76,7 @@ def main(ws, config_file, routing_model_name,
     return models_deployed
 
 
-def get_deployed_models(routing_model_name):
+def get_deployed_models(ws, routing_model_name):
 
     # Load deployed models info
     routing_model = Model.list(ws, name=routing_model_name, latest=True)
@@ -86,12 +91,11 @@ def get_deployed_models(routing_model_name):
         except WebserviceException:
             print(f'Webservice {service_name} not found')
 
-    # Exclude models associated to deleted werbservices
-    deployed_models = { model_name : {**model_info, 'service': services[model_info['webservice']]}
-                        for model_name, model_info in deployed_models.items()
-                        if model_info['webservice'] in services.keys() }
+    # Exclude models no longer deployed (associated to deleted webservices)
+    deployed_models = { model_name:model_info for model_name, model_info in deployed_models.items()
+                        if model_info['webservice'] in services }
 
-    return deployed_models
+    return deployed_models, services
 
 
 def get_models_in_groups(grouping_tags=[], sorting_tags=[], exclude_names=[], exclude_tags=[], 
@@ -134,10 +138,18 @@ def combine_tags(model, tags):
     return '-'.join([model.tags.get(t, '') for t in tags])
 
 
-def split_groups_new_updated(model_groups, deployed_models):
+def split_groups(model_groups, deployed_models):
     
     deployed_groups = set(m['group'] for m in deployed_models.values())
 
+    # Services to be deleted
+    services_todelete = {}
+    for model_name, m in deployed_models.items():
+        if m['group'] not in model_groups:
+            service_models = services_todelete.setdefault(m['webservice'], [])
+            service_models.append(model_name)
+
+    # Split groups: new / to be updated / not changed
     groups_new, groups_updated = {}, {}
     for group_name, group_models in model_groups.items():
         group_exists = group_name in deployed_groups
@@ -149,9 +161,11 @@ def split_groups_new_updated(model_groups, deployed_models):
         else:
             pass
 
-    print(f'{len(groups_new)} groups to be deployed, {len(groups_updated)} groups to be updated.')
+    print(f'{len(groups_new)} groups to be deployed,',
+          f'{len(groups_updated)} groups to be updated,',
+          f'{len(services_todelete)} services to be deleted.')
     
-    return groups_new, groups_updated
+    return groups_new, groups_updated, services_todelete
 
 
 def get_deployment_config(config_file, aks_target=None):
@@ -207,16 +221,16 @@ def get_webservice_config(config_file):
     return webservice_type, webservice_config
 
 
-def deploy_model_group(ws, group_name, group_models, deployment_config, name_prefix='manymodels-', update=False):
+def deploy_model_group(ws, group_name, group_models, deployment_config, existing_services={}, name_prefix='manymodels-'):
     
     service_name = '{prefix}{group}'.format(
         prefix=name_prefix,
         group=group_name
     ).lower()
 
-    if update:
+    if service_name in existing_services:
         print(f'Launching updating of {service_name}...')
-        service = Webservice(ws, service_name)
+        service = existing_services[service_name]
         service.update(
             models=group_models,
             inference_config=deployment_config['inference_config']
@@ -241,14 +255,14 @@ def parse_args(args=None):
     parser.add_argument('--subscription-id', required=True, type=str)
     parser.add_argument('--resource-group', required=True, type=str)
     parser.add_argument('--workspace-name', required=True, type=str)
-    parser.add_argument("--deploy-config-file", required=True, type=str)
-    parser.add_argument("--grouping-tags", default='', type=lambda str: [t for t in str.split(',') if t])
-    parser.add_argument("--sorting-tags", default='', type=lambda str: [t for t in str.split(',') if t])
-    parser.add_argument("--routing-model-name", type=str, default='deployed_models_info')
-    parser.add_argument("--output", type=str, default='models_deployed.pkl')
-    parser.add_argument("--aks-target", type=str)
-    parser.add_argument("--service-prefix", type=str)
-    parser.add_argument("--container-size", type=int, default=250)
+    parser.add_argument('--deploy-config-file', required=True, type=str)
+    parser.add_argument('--grouping-tags', default='', type=lambda str: [t for t in str.split(',') if t])
+    parser.add_argument('--sorting-tags', default='', type=lambda str: [t for t in str.split(',') if t])
+    parser.add_argument('--routing-model-name', type=str, default='deployed_models_info')
+    parser.add_argument('--output', type=str, default='models_deployed.pkl')
+    parser.add_argument('--aks-target', type=str)
+    parser.add_argument('--service-prefix', type=str)
+    parser.add_argument('--container-size', type=int, default=250)
     args_parsed = parser.parse_args(args)
 
     if args_parsed.aks_target == '':
