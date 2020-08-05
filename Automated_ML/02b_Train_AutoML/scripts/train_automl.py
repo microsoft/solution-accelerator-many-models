d61244940b29e3273f9c7d9e780999e4a8ef8489
Deepti Mokkapati <deeptim@microsoft.com>
2020-08-05 09:18:10 -0700

    add log

--- Automated_ML/02b_Train_AutoML/scripts/train_automl.py
+++ Automated_ML/02b_Train_AutoML/scripts/train_automl.py
@@ -191,8 +191,8 @@ def run(input_data):
             except Exception as error:
                 error_type = ErrorTypes.Unclassified
                 error_message = 'Failed to register the model. ' + 'Error message: ' + str(error)
-                import traceback
-                logger.info(traceback.print_exc())
+		from azureml.automl.core.shared import logging_utilities
+                logging_utilities.log_traceback(error, None)
                 logger.info(error_message)
 
             date2 = datetime.datetime.now()
