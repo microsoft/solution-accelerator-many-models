9bdc199b172b7e9f9fe5ebb24a4376e8b62fd4a2
María Medina <mariamedp@gmail.com>
2020-09-24 08:42:16 +0200

    Customscript train params in script settings file

--- scripts/customscript/train.py
+++ scripts/customscript/train.py
@@ -1,13 +1,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from azureml.core import Run
-import pandas as pd
-import numpy as np
 import os
+import json
 import argparse
 import datetime
 import joblib
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from azureml.core import Run
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.linear_model import LinearRegression
@@ -16,19 +19,24 @@ from utils.models import get_model_name
 from utils.timeseries import ColumnDropper, SimpleLagger, SimpleCalendarFeaturizer, SimpleForecaster
 
 
-# 0.0 Parse input arguments
-parser = argparse.ArgumentParser("split")
-parser.add_argument("--id_columns", type=str, nargs='*', required=True, help="input columns identifying the model entity")
-parser.add_argument("--target_column", type=str, required=True, help="input target column")
-parser.add_argument("--timestamp_column", type=str, required=True, help="input timestamp column")
-parser.add_argument("--tag_columns", type=str, nargs='*', default=[], help="input columns to set as tags for the model")
-parser.add_argument("--drop_id", action='store_true', help="flag to drop columns used as ID")
-parser.add_argument("--drop_tags", action='store_true', help="flag to drop columns used as tags")
-parser.add_argument("--drop_columns", type=str, nargs='*', default=[], help="list of columns to drop prior to modeling")
-parser.add_argument("--model_type", type=str, required=True, help="input model type")
-parser.add_argument("--test_size", type=int, required=True, help="number of observations to be used for testing")
-
+# 0.0 Parse input arguments and read settings
+parser = argparse.ArgumentParser()
+parser.add_argument("--settings-file", type=str, required=True, help="file containing the script settings")
 args, _ = parser.parse_known_args()
+
+base_path = Path(__file__).absolute().parent
+with open(os.path.join(base_path, args.settings_file), 'r') as f:
+    customscript_settings = json.load(f)
+
+id_columns = customscript_settings['id_columns']
+target_column = customscript_settings['target_column']
+timestamp_column = customscript_settings['timestamp_column']
+model_type = customscript_settings['model_type']
+tag_columns = customscript_settings.get('tag_columns', [])
+drop_id = customscript_settings.get('drop_id', False)
+drop_tags = customscript_settings.get('drop_tags', False)
+drop_columns = customscript_settings.get('drop_columns', [])
+test_size = customscript_settings.get('test_size', 10)
 
 
 def init():
@@ -49,32 +57,32 @@ def run(input_data):
         start_datetime = datetime.datetime.now()
 
         # 1.0 Read the data from CSV - parse timestamps as datetime type and put the time in the index
-        data = (pd.read_csv(csv_file_path, parse_dates=[args.timestamp_column], header=0)
-                .set_index(args.timestamp_column)
+        data = (pd.read_csv(csv_file_path, parse_dates=[timestamp_column], header=0)
+                .set_index(timestamp_column)
                 .sort_index(ascending=True))    
 
         # ID and tags: uses the values from the first row of data
-        id_dict = {id_col: str(data[id_col].iloc[0]) for id_col in args.id_columns}
-        model_name = get_model_name(args.model_type, id_dict)
-        tags_dict = {tag_col: str(data[tag_col].iloc[0]) for tag_col in args.tag_columns}
-        cols_todrop = args.drop_columns + \
-                      (args.id_columns if args.drop_id else []) + \
-                      (args.tag_columns if args.drop_tags else [])
+        id_dict = {id_col: str(data[id_col].iloc[0]) for id_col in id_columns}
+        model_name = get_model_name(model_type, id_dict)
+        tags_dict = {tag_col: str(data[tag_col].iloc[0]) for tag_col in tag_columns}
+        cols_todrop = drop_columns + \
+                      (id_columns if drop_id else []) + \
+                      (tag_columns if drop_tags else [])
         print(f'Model name: "{model_name}"')
         print(f'ID tags: {id_dict}')
         print(f'Extra tags: {tags_dict}')
         print(f'Columns to drop: {cols_todrop}')
 
         # 2.0 Split the data into train and test sets
-        train = data[:-args.test_size]
-        test = data[-args.test_size:]
+        train = data[:-test_size]
+        test = data[-test_size:]
 
         # 3.0 Create and fit the forecasting pipeline
         # The pipeline will drop unhelpful features, make a calendar feature, and make lag features
-        lagger = SimpleLagger(args.target_column, lag_orders=[1, 2, 3, 4])
+        lagger = SimpleLagger(target_column, lag_orders=[1, 2, 3, 4])
         transform_steps = [('column_dropper', ColumnDropper(cols_todrop)),
                            ('calendar_featurizer', SimpleCalendarFeaturizer()), ('lagger', lagger)]
-        forecaster = SimpleForecaster(transform_steps, LinearRegression(), args.target_column, args.timestamp_column)
+        forecaster = SimpleForecaster(transform_steps, LinearRegression(), target_column, timestamp_column)
         forecaster.fit(train)
         print('Featurized data example:')
         print(forecaster.transform(train).head())
@@ -84,10 +92,10 @@ def run(input_data):
         compare_data = test.assign(forecasts=forecasts).dropna()
 
         # 5.0 Calculate accuracy metrics for the fit
-        mse = mean_squared_error(compare_data[args.target_column], compare_data['forecasts'])
+        mse = mean_squared_error(compare_data[target_column], compare_data['forecasts'])
         rmse = np.sqrt(mse)
-        mae = mean_absolute_error(compare_data[args.target_column], compare_data['forecasts'])
-        actuals = compare_data[args.target_column].values
+        mae = mean_absolute_error(compare_data[target_column], compare_data['forecasts'])
+        actuals = compare_data[target_column].values
         preds = compare_data['forecasts'].values
         mape = np.mean(np.abs((actuals - preds) / actuals) * 100)
 
@@ -106,14 +114,14 @@ def run(input_data):
         current_run.upload_file(model_name, model_path)
 
         # 9.0 Register the model to the workspace to be used in forecasting
-        all_tags = {'ModelType': args.model_type, **id_dict, **tags_dict}
+        all_tags = {'ModelType': model_type, **id_dict, **tags_dict}
         current_run.register_model(model_path=model_name, model_name=model_name,
-                                   model_framework=args.model_type, tags=all_tags)
+                                   model_framework=model_type, tags=all_tags)
 
         # 10.0 Add data to output
         end_datetime = datetime.datetime.now()
         result.update(id_dict)
-        result['model_type'] = args.model_type
+        result['model_type'] = model_type
         result['file_name'] = csv_file_path
         result['model_name'] = model_name
         result['start_date'] = str(start_datetime)
