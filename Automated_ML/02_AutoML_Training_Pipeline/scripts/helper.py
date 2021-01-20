edf24a81c406dcdfdbe5f2a2a8dc75100d52312d
Deepti Mokkapati <deeptim@microsoft.com>
2021-01-20 10:57:33 -0800

    move to contrib package

--- Automated_ML/02_AutoML_Training_Pipeline/scripts/helper.py
+++ Automated_ML/02_AutoML_Training_Pipeline/scripts/helper.py
@@ -2,18 +2,12 @@
 # Licensed under the MIT License.
 
 
-import json
 import sys
 
 from azureml.core import Experiment
 from azureml.core.run import Run
 from azureml.core import Workspace
 sys.path.append("..")
-
-
-def write_automl_settings_to_file(automl_settings):
-    with open('scripts//automlconfig.json', 'w', encoding='utf-8') as f:
-        json.dump(automl_settings, f, ensure_ascii=False, indent=4)
 
 
 def cancel_runs_in_experiment(ws, experiment):
@@ -27,24 +21,6 @@ def cancel_runs_in_experiment(ws, experiment):
                 run.cancel()
         except Exception as e:
             print('Canceling run failed due to ', e)
-
-
-def build_parallel_run_config(train_env, compute, nodecount, workercount, timeout):
-    from azureml.pipeline.steps import ParallelRunConfig
-    from common.scripts.helper import validate_parallel_run_config
-    parallel_run_config = ParallelRunConfig(
-        source_directory='./scripts',
-        entry_script='train_minibatch.py',
-        mini_batch_size="1",  # do not modify this setting
-        run_invocation_timeout=timeout,
-        error_threshold=-1,
-        output_action="append_row",
-        environment=train_env,
-        process_count_per_node=workercount,
-        compute_target=compute,
-        node_count=nodecount)
-    validate_parallel_run_config(parallel_run_config)
-    return parallel_run_config
 
 
 def get_automl_environment(workspace: Workspace, automl_settings_dict: dict):
